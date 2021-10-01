3f83c6ef05a7efaf927b7efa1e627dbd166e70c7
jderen <kuba35groszy@tlen.pl>
2021-10-01 22:51:45 +0200

    first commit

---
+++ SzczurTV/src/test/java/com/example/szczurtv/szczurtv/SzczurTvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szczurtv.szczurtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzczurTvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
